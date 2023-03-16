f9fbf7dae2ba9bb48969d1d3c530f4819b50ec58
esfalsa <pronoun.nationstates@gmail.com>
2023-03-16 15:49:57 -0700

    chore: remove unused dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react()],
   base: "/libcord-crossing",
   resolve: {
     alias: {
