29bd60cae5ceedd124ae62f78de3b4f673b38c3d
Pronoun <96925249+esfalsa@users.noreply.github.com>
2023-03-17 20:37:37 -0700

    fix: trim nation names (#10)

--- src/utils/nation-names.ts
+++ src/utils/nation-names.ts
@@ -1,9 +1,9 @@
 import { titleCase } from "title-case";
 
 export function canonicalize(nation: string) {
-  return nation.toLowerCase().replaceAll(" ", "_");
+  return nation.trim().toLowerCase().replaceAll(/\s+/g, "_");
 }
 
 export function prettify(nation: string) {
-  return titleCase(nation.replaceAll("_", " "));
+  return titleCase(nation.trim().replaceAll("_", " "));
 }
